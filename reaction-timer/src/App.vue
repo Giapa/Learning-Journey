507da98ee12531f0b409f7ec77a88efd64c6bcc9
Giapa <giapa@protonmail.com>
2021-04-23 16:35:57 +0300

    Add rank and styles

--- reaction-timer/src/App.vue
+++ reaction-timer/src/App.vue
@@ -44,4 +44,19 @@ export default {
   color: #444;
   margin-top: 60px;
 }
+button {
+  background: #0faf87;
+  color: white;
+  border: none;
+  padding: 8px 16px;
+  border-radius: 4px;
+  font-size: 16px;
+  letter-spacing: 1px;
+  cursor: pointer;
+  margin: 10px;
+}
+button[disabled] {
+  opacity: 0.2;
+  cursor: not-allowed;
+}
 </style>
--- reaction-timer/src/components/Results.vue
+++ reaction-timer/src/components/Results.vue
@@ -1,11 +1,32 @@
 <template>
   <p>Score is: {{ score }} ms</p>
+  <p>Your rank is: {{ rank }}</p>
 </template>
 
 <script>
 export default {
   props: ["score"],
+  data() {
+    return {
+      rank: null,
+    };
+  },
+  mounted() {
+    if (this.score < 250) {
+      this.rank = "How is that possible";
+    } else if (this.score < 400) {
+      this.rank = "Rapid reflexes";
+    } else {
+      this.rank = "Too slow";
+    }
+  },
 };
 </script>
 
-<style></style>
+<style>
+.rank {
+  font-size: 1.4rem;
+  color: #0faf87;
+  font-weight: bold;
+}
+</style>
