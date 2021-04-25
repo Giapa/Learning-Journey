87e90351a82b996494fc4e28d62a00d3d920191c
Giapa <giapa@protonmail.com>
2021-04-26 00:53:01 +0300

    Add event listeners and arrays

--- Vue/web-form/src/components/SignupForm.vue
+++ Vue/web-form/src/components/SignupForm.vue
@@ -9,7 +9,11 @@
       <option value="developer">Web developer</option>
       <option value="designer">Web designer</option>
     </select>
-
+    <label>Skills</label>
+    <input type="text" v-model="tempSkill" @keyup="addSkill" />
+    <div v-for="skill in skills" :key="skill" class="pill">
+      <span @click="deleteSkill(skill)">{{ skill }}</span>
+    </div>
     <div class="terms">
       <input type="checkbox" v-model="terms" required />
       <label>Accept terms and conditions</label>
@@ -29,7 +33,24 @@ export default {
       password: "",
       role: "",
       terms: false,
+      tempSkill: "",
+      skills: [],
     };
+  },
+  methods: {
+    addSkill(e) {
+      if (e.key === "," && this.tempSkill) {
+        if (!this.skills.includes(this.tempSkill)) {
+          this.skills.push(this.tempSkill.replace(",", ""));
+        }
+        this.tempSkill = "";
+      }
+    },
+    deleteSkill(skill) {
+      this.skills = this.skills.filter((item) => {
+        return item !== skill;
+      });
+    },
   },
 };
 </script>
@@ -68,5 +89,17 @@ input[type="checkbox"] {
   margin: 0 10px 0 0;
   position: relative;
   top: 2px;
+}
+.pill {
+  display: inline-block;
+  margin: 20px 10px 0 0;
+  padding: 6px 12px;
+  background: #eee;
+  border-radius: 20px;
+  font-size: 12px;
+  letter-spacing: 1px;
+  font-weight: bold;
+  color: #777;
+  cursor: pointer;
 }
 </style>
