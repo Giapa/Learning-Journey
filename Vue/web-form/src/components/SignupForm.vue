95a6713d399e9735b1ae39d1a74bf8160f1375f4
Giapa <giapa@protonmail.com>
2021-04-26 00:19:24 +0300

    Add data binding

--- Vue/web-form/src/components/SignupForm.vue
+++ Vue/web-form/src/components/SignupForm.vue
@@ -1,12 +1,37 @@
 <template>
   <form>
     <label>Email</label>
-    <input type="email" required />
+    <input type="email" required v-model="email" />
+    <label>Password</label>
+    <input type="password" required v-model="password" />
+    <label>Role:</label>
+    <select v-model="role">
+      <option value="developer">Web developer</option>
+      <option value="designer">Web designer</option>
+    </select>
+
+    <div class="terms">
+      <input type="checkbox" v-model="terms" required />
+      <label>Accept terms and conditions</label>
+    </div>
   </form>
+  <p>Email: {{ email }}</p>
+  <p>Password: {{ password }}</p>
+  <p>Role: {{ role }}</p>
+  <p>Terms: {{ terms }}</p>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      email: "",
+      password: "",
+      role: "",
+      terms: false,
+    };
+  },
+};
 </script>
 
 <style>
@@ -27,7 +52,8 @@ label {
   letter-spacing: 1px;
   font-weight: bold;
 }
-input {
+input,
+select {
   display: block;
   padding: 10px 6px;
   width: 100%;
@@ -35,5 +61,12 @@ input {
   border: none;
   border-bottom: 1px solid #ddd;
   color: #555;
+}
+input[type="checkbox"] {
+  display: inline-block;
+  width: 16px;
+  margin: 0 10px 0 0;
+  position: relative;
+  top: 2px;
 }
 </style>
