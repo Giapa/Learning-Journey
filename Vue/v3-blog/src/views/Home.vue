1a492092d0d4c186579fc5fb74377d7885b77eb0
Giapa <giapa@protonmail.com>
2021-05-04 23:32:11 +0300

    Add template refs

--- Vue/v3-blog/src/views/Home.vue
+++ Vue/v3-blog/src/views/Home.vue
@@ -1,26 +1,31 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <p>{{ name }}</p>
+    <p ref="p">{{ name }}</p>
     <p>{{ age }}</p>
     <button @click="handleClick">Click me</button>
   </div>
 </template>
 
 <script>
+import { ref } from "@vue/reactivity";
 export default {
   name: "Home",
   setup() {
     console.log("Setup");
 
+    const p = ref(null);
+
     let name = "Mario";
     let age = 30;
 
     const handleClick = () => {
-      console.log("you clicked the button");
+      console.log(p, p.value);
+      p.value.classList.add("test");
+      p.value.textContent = "This text is replaced by reference";
     };
 
-    return { name, age, handleClick };
+    return { name, age, handleClick, p };
   },
 };
 </script>
