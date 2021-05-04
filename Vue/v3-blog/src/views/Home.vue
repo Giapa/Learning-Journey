e752402448035096245492e3e74e394fa9b3f265
Giapa <giapa@protonmail.com>
2021-05-04 23:24:35 +0300

    Check basics of composition API

--- Vue/v3-blog/src/views/Home.vue
+++ Vue/v3-blog/src/views/Home.vue
@@ -1,10 +1,26 @@
 <template>
-  <div class="home"></div>
+  <div class="home">
+    <h1>Home</h1>
+    <p>{{ name }}</p>
+    <p>{{ age }}</p>
+    <button @click="handleClick">Click me</button>
+  </div>
 </template>
 
 <script>
 export default {
   name: "Home",
-  components: {},
+  setup() {
+    console.log("Setup");
+
+    let name = "Mario";
+    let age = 30;
+
+    const handleClick = () => {
+      console.log("you clicked the button");
+    };
+
+    return { name, age, handleClick };
+  },
 };
 </script>
