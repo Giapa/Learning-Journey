76310568fe21ac1122d6a09d4ae46b6734445c39
Giapa <giapa@protonmail.com>
2021-05-06 23:44:04 +0300

    Add reactive objects

--- Vue/v3-blog/src/views/Home.vue
+++ Vue/v3-blog/src/views/Home.vue
@@ -1,16 +1,22 @@
 <template>
   <div class="home">
     <h1>Home</h1>
+    <h2>Refs</h2>
     <p>{{ name }}</p>
     <p>{{ age }}</p>
     <button @click="handleClick">Click me</button>
     <button @click="age++">Add 1 to age</button>
     <input type="text" v-model="name" />
+    <br />
+    <h2>Reactive</h2>
+    <p>{{ reactivename.name }}</p>
+    <p>{{ reactivename.age }}</p>
+    <button @click="updateReactive">Click here to change hero</button>
   </div>
 </template>
 
 <script>
-import { ref } from "@vue/reactivity";
+import { ref, reactive } from "@vue/reactivity";
 export default {
   name: "Home",
   setup() {
@@ -19,12 +25,19 @@ export default {
     const name = ref("Mario");
     const age = ref(30);
 
+    const reactivename = reactive({ name: "Peach", age: 25 });
+
     const handleClick = () => {
       age.value = 40;
       name.value = "Luigi";
     };
 
-    return { name, age, handleClick };
+    const updateReactive = () => {
+      reactivename.name = "Daisy";
+      reactivename.age = "27";
+    };
+
+    return { name, age, handleClick, reactivename, updateReactive };
   },
 };
 </script>
