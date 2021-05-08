f28a9fcd32e920163f3a4a807378aa1ff59883f7
Giapa <giapa@protonmail.com>
2021-05-08 18:22:45 +0300

    Add composables

---
+++ Vue/v3-blog/src/composables/getPosts.js
@@ -0,0 +1,23 @@
+import { ref } from "vue";
+
+const getPosts = () => {
+  const posts = ref([]);
+  const error = ref(null);
+
+  const load = async () => {
+    try {
+      let data = await fetch("http://localhost:3000/posts");
+      if (!data.ok) {
+        throw Error("No data available");
+      }
+      posts.value = await data.json();
+    } catch (err) {
+      error.value = err.message;
+      console.log(error.value);
+    }
+  };
+
+  return { posts, error, load };
+};
+
+export default getPosts;
