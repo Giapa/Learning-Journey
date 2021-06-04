57310701c35f5a4e637e6062fab9054433aa7007
Giapa <giapa@protonmail.com>
2021-06-04 14:47:55 +0300

    Simple form with tailwind

---
+++ Vue/tailwind-components/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="min-h-screen bg-grey-darker p-8">
+    <div class="max-w-sm mx-auto">
+      <user-settings-form></user-settings-form>
+    </div>
+  </div>
+</template>
+
+<script>
+import UserSettingsForm from "./components/UserSettingsForm.vue"
+
+export default {
+  name: 'App',
+  components: {
+    UserSettingsForm,
+  }
+}
+</script>
+
+<style src="./assets/css/app.css"/>
