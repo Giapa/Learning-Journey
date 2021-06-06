7ac2d168bc889e74049e2c1eb4d0446db0bd9756
Giapa <giapa@protonmail.com>
2021-06-06 20:57:30 +0300

    Add nav and basic front page

--- Vue/casino/src/App.vue
+++ Vue/casino/src/App.vue
@@ -1,15 +1,21 @@
 <template>
   <div id="app">
-    <div id="nav">
+    <nav-bar></nav-bar>
+    <!-- <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
-    </div>
+    </div> -->
     <router-view />
-    <div class="flex justify-center items-center h-screen">
-      <h1 class="font-bold text-9xl text-green-600">This is some text</h1>
-    </div>
   </div>
 </template>
+
+<script>
+import NavBar from "./components/NavBar.vue";
+
+export default {
+  components: { NavBar },
+};
+</script>
 
 <style lang="scss">
 #app {
@@ -18,18 +24,6 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+  margin: 0;
 }
 </style>
--- Vue/casino/src/views/Home.vue
+++ Vue/casino/src/views/Home.vue
@@ -1,18 +1,89 @@
 <template>
-  <div class="home grid grid-flow-row">
-    <img alt="Vue logo" src="../assets/logo.png" class="mx-auto" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <h1
+      class="
+        text-4xl
+        sm:text-6xl
+        lg:text-7xl
+        leading-none
+        font-extrabold
+        tracking-tight
+        text-gray-900
+        mt-10
+        mb-8
+        sm:mt-14 sm:mb-10
+        inline-flex
+        items-center
+      "
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="h-12 w-20 inline-block"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+        />
+      </svg>
+      Money Loss.
+    </h1>
+    <p
+      class="
+        text-lg
+        sm:text-2xl sm:leading-10
+        font-medium
+        mb-10
+        sm:mb-11
+        text-center
+      "
+    >
+      Your pain is our gain. Play to lose.
+    </p>
+    <a
+      class="
+        w-full
+        sm:w-auto
+        flex-none
+        bg-gray-900
+        hover:bg-gray-700
+        text-white text-lg
+        leading-6
+        font-semibold
+        py-3
+        px-6
+        border border-transparent
+        rounded-xl
+        focus:ring-2
+        focus:ring-offset-2
+        focus:ring-offset-white
+        focus:ring-gray-900
+        focus:outline-none
+        transition-colors
+        duration-200
+      "
+      href=""
+      >Start losing</a
+    >
+    <h1 class="my-10">
+      More than 100.000.000 have lost their entire property. Don't miss out on
+      the feeling.
+    </h1>
+    <user-card>
+      <template v-slot:name> Gregory Giapa </template>
+      I lost everything to my gabling addiction
+    </user-card>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import UserCard from "../components/UserCard.vue";
 export default {
   name: "Home",
-  components: {
-    HelloWorld,
-  },
+  components: { UserCard },
 };
 </script>
