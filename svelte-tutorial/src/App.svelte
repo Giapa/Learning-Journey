aeceb09253bf0f55ff6881c8ed33704ebf88d601
Giapa <giapa@protonmail.com>
2021-06-27 22:09:19 +0300

    Finish todo list

--- svelte-tutorial/src/App.svelte
+++ svelte-tutorial/src/App.svelte
@@ -1,30 +1,11 @@
 <script>
-	export let name;
+	import TodoList from './Components/TodoList.svelte';
+	let todos = [{text: "Go to super", status: false}, {text: "Make food", status: true},{text: "Study", status: false}]
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<TodoList todos={todos}/>
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
 </style>
